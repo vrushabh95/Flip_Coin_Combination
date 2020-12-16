e1fc25ef8b9f1bc45c2f579f390a11675e2e6514
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-16 10:50:46 +0530

    Determining The Percentage Of Singlet Combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -6,10 +6,41 @@ function flipCoin() {
 	face=$(( RANDOM % 2 ))
 	if [ $face -eq 1 ]
 	then
-		echo "Heads"
+		echo "H"
 	else
-		echo "Tails"
+		echo "T"
 	fi
 }
-flipCoin
+
+function percentage() {
+	temp=$1
+	percent=$(( temp * 100 / 10 ))
+	echo "$percent%"
+}
+
+function oneFlip() {
+	declare -A singlet
+	singlet=( ["H"]=0 ["T"]=0 )
+	
+	singletHeadCount=0
+	singletTailCount=0
+	for (( i=0; i<10; i++ ))
+	do
+		result=$( flipCoin )
+		if [ $result == H ]
+		then
+			(( singletHeadCount++ ))
+			singlet[H]=$singletHeadCount
+		else
+			(( singletTailCount++ ))
+			singlet[T]=$singletTailCount
+		fi
+	done
+
+echo The Percentage of Head in singlet combination is $( percentage $singletHeadCount )
+echo The Percentage of Tail in singlet combination is $( percentage $singletTailCount )
+}
+
+oneFlip
+
 
