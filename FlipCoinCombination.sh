8f60cd60d372121dcf3ffd8716497d558715b84e
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-16 11:03:18 +0530

    Determining The Percentage Of Doublet Combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -37,10 +37,51 @@ function oneFlip() {
 		fi
 	done
 
-echo The Percentage of Head in singlet combination is $( percentage $singletHeadCount )
-echo The Percentage of Tail in singlet combination is $( percentage $singletTailCount )
+echo Head percent: $( percentage $singletHeadCount ), Tail percent: $( percentage $singletTailCount )
 }
 
-oneFlip
+function twoFlip() {
+	declare -A doublet
+	doublet=( ["HH"]=0 ["TT"]=0 ["HT"]=0 ["TH"]=0 )
 
+	doubleHeadCount=0
+	doubleTailCount=0
+	headThenTailCount=0
+	tailThenHeadCount=0
+	for (( j=0; j<10; j++ ))
+	do
+		result1=$( flipCoin )
+		result2=$( flipCoin )
+		if [ $result1$result2 == HH ]
+		then
+			(( doubleHeadCount++ ))
+			doublet[HH]=$doubleHeadCount
+		elif [ $result1$result2 == TT ]
+		then
+			(( doubleTailCount++ ))
+			doublet[TT]=$doubleTailCount
+		elif [ $result1$result2 == HT ]
+		then
+			(( headThenTailCount++ ))
+			doublet[HT]=$headThenTailCount
+		else
+			(( tailThenHeadCount++ ))
+			doublet[TH]=$tailThenHeadCount
+		fi
+	done
 
+echo HH percent: $( percentage $doubleHeadCount ), TT percent: $( percentage $doubleTailCount ), HT percent: $( percentage $headThenTailCount ), TH percent: $( percentage $tailThenHeadCount ) 
+}
+
+echo "----------Flip Coin Simulation----------"
+echo -e "Enter 1 to flip one coin \nEnter 2 to flip two coin"
+read -p "Enter choice: " choice
+
+case $choice in
+	1)
+		oneFlip;;
+	2)
+		twoFlip;;
+	*)
+		echo "Invalid choice!!";;
+esac 
